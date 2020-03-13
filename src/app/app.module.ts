4ab9025c0fbc14308e3af04b09aaca7e684e7e01
Rafi <rafimrm@gmail.com>
2020-03-13 15:24:53 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BillDetailComponent } from './bill-detail/bill-detail.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { SalesService } from 'src/app/service/sales.service';
+import { HttpClientModule } from '@angular/common/http';
+import { EmitterService } from 'src/app/emitter.service';
+import { CurrencyPipe } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillDetailComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
+    NgbModule ,HttpClientModule ],
+  providers: [SalesService,EmitterService,CurrencyPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
